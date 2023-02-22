058df611ca62fad7c741574068db5a79da1ddd2d
Ilango <rajagopalilango@gmail.com>
2023-02-22 21:10:22 +0530

    🐛 FIX: Fix 404 error because of theme (temporary fix)

--- docs/app/root.tsx
+++ docs/app/root.tsx
@@ -1,10 +1,4 @@
-import type {
-	LinksFunction,
-	LoaderArgs,
-	MetaFunction,
-	SerializeFrom,
-} from '@remix-run/node'
-import { json } from '@remix-run/node'
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
 import {
 	Links,
 	LiveReload,
@@ -12,14 +6,11 @@ import {
 	Outlet,
 	Scripts,
 	ScrollRestoration,
-	useLoaderData,
 } from '@remix-run/react'
 import { RescribeProvider } from 'rescribe'
 import config from '~/rescribe.config'
 import stylesheet from '~/main.css'
 import rescribeStylesheet from 'rescribe/main.css'
-import { ThemeHead, ThemeProvider, useTheme } from './utils/theme-provider'
-import { getThemeSession } from './utils/theme.server'
 
 export const links: LinksFunction = () => [
 	{ rel: 'stylesheet', href: stylesheet },
@@ -32,30 +23,15 @@ export const meta: MetaFunction = () => ({
 	viewport: 'width=device-width,initial-scale=1',
 })
 
-export type LoaderData = SerializeFrom<typeof loader>
-
-export async function loader({ request }: LoaderArgs) {
-	const themeSession = await getThemeSession(request)
-	return json({
-		theme: themeSession.getTheme(),
-	})
-}
-
 function App() {
-	const data = useLoaderData<LoaderData>()
-	const [theme] = useTheme()
-
 	return (
-		<html lang='en' className={`h-screen w-screen ${theme ?? ''}`}>
+		<html lang='en'>
 			<head>
 				<Meta />
 				<Links />
-				<ThemeHead ssrTheme={Boolean(data.theme)} />
 			</head>
-			<body className='h-full w-full bg-white dark:bg-[#040303]'>
-				<RescribeProvider config={config}>
-					<Outlet />
-				</RescribeProvider>
+			<body>
+				<Outlet />
 				<ScrollRestoration />
 				<Scripts />
 				<LiveReload />
@@ -65,10 +41,9 @@ function App() {
 }
 
 export default function AppWithProviders() {
-	const data = useLoaderData<LoaderData>()
 	return (
-		<ThemeProvider specifiedTheme={data.theme}>
+		<RescribeProvider config={config}>
 			<App />
-		</ThemeProvider>
+		</RescribeProvider>
 	)
 }
