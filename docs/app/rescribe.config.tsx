a876d2f21e60b5979bbf97c298c3dc37d51e93f1
Ilango <rajagopalilango@gmail.com>
2023-02-23 00:44:01 +0530

    👌 IMPROVE: Add sidebar links and radix icons

--- docs/app/rescribe.config.tsx
+++ docs/app/rescribe.config.tsx
@@ -1,6 +1,7 @@
-import { Switch } from '@i4o-oss/catalystui'
 import type { RescribeConfig } from 'rescribe'
+import { Switch } from '@i4o-oss/catalystui'
 import { Theme, useTheme } from '~/utils/theme-provider'
+import { FileTextIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
 
 function DarkModeToggle() {
 	const [theme, setTheme] = useTheme()
@@ -23,7 +24,19 @@ function DarkModeToggle() {
 const config: RescribeConfig = {
 	logo: 'https://raw.githubusercontent.com/i4o-oss/rescribe/main/docs/public/rescribe_logo.png',
 	sidebar: {
-		links: [],
+		links: [
+			{
+				href: '/docs',
+				icon: <FileTextIcon className='h-4 w-4' />,
+				label: 'Documentation',
+			},
+			{
+				external: true,
+				href: 'https://github.com/i4o-oss/rescribe',
+				icon: <GitHubLogoIcon className='h-4 w-4' />,
+				label: 'Github',
+			},
+		],
 		search: true,
 	},
 	theme: {
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
 	content: ['./app/**/*.{ts,tsx,mdx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brand: {
+					DEFAULT: '#2CB67D',
+					50: '#E6F9F1',
+					100: '#D2F4E6',
+					200: '#A5E9CD',
+					300: '#77DEB3',
+					400: '#46D298',
+					500: '#2CB67D',
+					600: '#239062',
+					700: '#1B6F4C',
+					800: '#124A33',
+					900: '#092519',
+				},
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+				serif: ['Merriweather', 'serif'],
+			},
+		},
 	},
 	plugins: [],
 }
