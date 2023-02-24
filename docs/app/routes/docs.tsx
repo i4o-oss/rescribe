4537e33d6ecb6b307a5830c67cca963ad9f69c2b
Ilango <rajagopalilango@gmail.com>
2023-02-25 01:44:12 +0530

    📦 NEW: Fetch data required for rescribe using loaders

--- docs/app/routes/docs.tsx
+++ docs/app/routes/docs.tsx
@@ -1,5 +1,19 @@
-import { Outlet } from '@remix-run/react'
+import { json, LoaderArgs } from '@remix-run/node'
+import { Outlet, useLoaderData } from '@remix-run/react'
+import { Layout, getMdxHeadings } from 'rescribe'
+
+export async function loader({ request }: LoaderArgs) {
+	const url = new URL(request.url)
+	const headings = await getMdxHeadings(url.pathname)
+	return json({ headings })
+}
 
 export default function Docs() {
-	return <Outlet />
+	const data = useLoaderData()
+
+	return (
+		<Layout data={data}>
+			<Outlet />
+		</Layout>
+	)
 }
--- docs/remix.config.js
+++ docs/remix.config.js
@@ -4,9 +4,19 @@ module.exports = {
 		unstable_tailwind: true,
 	},
 	ignoredRouteFiles: ['**/.*'],
+	mdx: async () => {
+		const [rehypeSlug] = await Promise.all([
+			import('rehype-slug').then((mod) => mod.default),
+		])
+
+		return {
+			rehypePlugins: [rehypeSlug],
+		}
+	},
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
 	// serverBuildPath: "build/index.js",
 	// publicPath: "/build/",
+	serverDependenciesToBundle: [/.*/],
 	watchPaths: ['../packages/rescribe/'],
 }
