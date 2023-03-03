053e0e6f35ce2ea9dc6416adea6f18dba3e01bdd
Ilango <rajagopalilango@gmail.com>
2023-03-03 10:02:43 +0530

    👌 IMPROVE: Update config

--- docs/app/rescribe.config.tsx
+++ docs/app/rescribe.config.tsx
@@ -5,6 +5,7 @@ import {
 	DashboardIcon,
 	FileTextIcon,
 	GitHubLogoIcon,
+	TwitterLogoIcon,
 } from '@radix-ui/react-icons'
 
 function DarkModeToggle() {
@@ -26,9 +27,68 @@ function DarkModeToggle() {
 }
 
 const config: RescribeConfig = {
+	footer: {
+		socials: [
+			{
+				ariaLabel: 'Github Repository',
+				icon: <GitHubLogoIcon className='h-6 w-6' />,
+				href: 'https://github.com/i4o-oss/rescribe',
+			},
+			{
+				ariaLabel: 'Twitter Profile',
+				icon: <TwitterLogoIcon className='h-6 w-6' />,
+				href: 'https://twitter.com/i4o_dev',
+			},
+		],
+		text: (
+			<>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					width='24'
+					height='24'
+					viewBox='0 0 24 24'
+					fill='none'
+					stroke='currentColor'
+					strokeWidth='2'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+				>
+					<path d='m7 11 2-2-2-2'></path>
+					<path d='M11 13h4'></path>
+					<rect
+						x='3'
+						y='3'
+						width='18'
+						height='18'
+						rx='2'
+						ry='2'
+					></rect>
+				</svg>
+				<p>
+					Built by{' '}
+					<a
+						className='underline'
+						href='https://i4o.dev'
+						target='_blank'
+						rel='noreferrer noopener'
+					>
+						i4o
+					</a>
+					.
+				</p>
+			</>
+		),
+	},
 	navbar: {
 		logo: 'https://raw.githubusercontent.com/i4o-oss/rescribe/main/docs/public/rescribe_logo.png',
-		search: true,
+		search: false,
+		socials: [
+			{
+				ariaLabel: 'Github Repository',
+				icon: <GitHubLogoIcon className='h-6 w-6' />,
+				href: 'https://github.com/i4o-oss/rescribe',
+			},
+		],
 	},
 	sidebar: {
 		links: [
