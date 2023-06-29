fc3f08428e7897a88dfd469499155dbafc5e6e9f
Ilango <rajagopalilango@gmail.com>
2023-06-29 17:00:45 +0530

    📦 NEW: Add functions for getting headings when using v2 routing convention in remix

--- packages/rescribe/src/utils/fs.ts
+++ packages/rescribe/src/utils/fs.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import slash from 'slash'
 import matter from 'gray-matter'
 import { MARKDOWN_EXTENSION_REGEX, REMIX_ROUTES_DIR } from '../constants'
-import { Folder } from '../types'
+import type { Folder } from '../types'
 
 async function collectMdx(filePath: string) {
 	const content = await fs.promises.readFile(filePath, 'utf8')
@@ -16,38 +16,66 @@ async function collectMdx(filePath: string) {
 
 async function readFilesInDir(
 	pathname: string,
+	v2 = false,
 	dir = REMIX_ROUTES_DIR,
 	route = '/'
 ): Promise<Array<Folder>> {
 	const files = await fs.promises.readdir(dir, { withFileTypes: true })
 
-	const items = await Promise.all(
-		files.map(async (f) => {
-			const filePath = path.join(dir, f.name)
-			const isDir = f.isDirectory()
-			const base = path.parse(f.name).base
-			const { name, ext } = isDir
-				? { name: base, ext: '' }
-				: parseFileName(filePath)
-			const fileRoute = normalizePageRoute(route, name)
+	if (!v2) {
+		const items = await Promise.all(
+			files.map(async (f) => {
+				const filePath = path.join(dir, f.name)
+				const isDir = f.isDirectory()
+				const base = path.parse(f.name).base
+				const { name, ext } = isDir
+					? { name: base, ext: '' }
+					: parseFileName(filePath)
+				const fileRoute = normalizePageRoute(route, name)
 
-			if (pathname.startsWith(fileRoute)) {
-				if (isDir) {
-					return {
-						base,
-						path: filePath,
-						route: fileRoute,
-						children: await readFilesInDir(
-							pathname,
-							path.join(dir, f.name),
-							fileRoute
-						),
+				if (pathname.startsWith(fileRoute)) {
+					if (isDir) {
+						return {
+							base,
+							path: filePath,
+							route: fileRoute,
+							children: await readFilesInDir(
+								pathname,
+								v2,
+								path.join(dir, f.name),
+								fileRoute
+							),
+						}
+					}
+
+					if (
+						MARKDOWN_EXTENSION_REGEX.test(ext) &&
+						fileRoute === pathname
+					) {
+						return {
+							base,
+							path: filePath,
+							route: fileRoute,
+							data: await collectMdx(filePath),
+						}
 					}
 				}
+			})
+		)
 
+		return items as Array<Folder>
+	} else {
+        const pathnameParts = pathname.split('/').filter((e) => e !== '')
+		const filename = pathnameParts.join('.')
+		const items = await Promise.all(
+			files.map(async (f) => {
+				const filePath = path.join(dir, f.name)
+				const base = path.parse(f.name).base
+				const { name, ext } = parseFileName(base)
+				const fileRoute = normalizePageRoute(route, name)
 				if (
-					MARKDOWN_EXTENSION_REGEX.test(ext) &&
-					fileRoute === pathname
+					name === filename &&
+					MARKDOWN_EXTENSION_REGEX.test(ext)
 				) {
 					return {
 						base,
@@ -55,12 +83,23 @@ async function readFilesInDir(
 						route: fileRoute,
 						data: await collectMdx(filePath),
 					}
-				}
-			}
-		})
-	)
+				} else if (
+                    pathnameParts.length === 1 &&
+                    name.startsWith(pathnameParts[0]) &&
+                    name.endsWith('_index')
+                ) {
+					return {
+						base,
+						path: filePath,
+						route: fileRoute,
+						data: await collectMdx(filePath),
+					}
+                }
+			})
+		)
 
-	return items as Array<Folder>
+		return items as Array<Folder>
+	}
 }
 
 function parseFileName(fp: string) {
--- packages/rescribe/src/utils/mdx.ts
+++ packages/rescribe/src/utils/mdx.ts
@@ -4,35 +4,55 @@ import { createProcessor } from '@mdx-js/mdx'
 import remarkHeadings from './plugins/remark-headings'
 import { readFilesInDir } from './fs'
 
-async function readCurrentMdx(pathname: string): Promise<VFile> {
-	const matchedRoutes = await readFilesInDir(pathname)
+async function readCurrentMdx({
+    pathname,
+    v2 = false,
+}: {
+    pathname: string
+    v2?: boolean
+}): Promise<VFile> {
+    const matchedRoutes = await readFilesInDir(pathname, v2)
 
-	const [matchedRoute] = matchedRoutes?.filter((item) => item)
-	// @ts-ignore
-	const [currentFile] = matchedRoute?.children?.filter(
-		(child: unknown | null) => child
-	)
-	const input = await fs.promises.readFile(currentFile.path, 'utf8')
-	const processor = createProcessor({
-		remarkPlugins: [remarkHeadings],
-	})
-	const vfile = await processor.process(input)
+    const [matchedRoute] = matchedRoutes?.filter((item) => item)
+    // @ts-ignore
+    const [currentFile] = v2 ? [matchedRoute] : matchedRoute?.children?.filter(
+        (child: unknown | null) => child
+    )
+    const input = await fs.promises.readFile(currentFile.path, 'utf8')
+    const processor = createProcessor({
+        remarkPlugins: [remarkHeadings],
+    })
+    const vfile = await processor.process(input)
 
-	return vfile
+    return vfile
 }
 
 async function getMdxHeadings(request: Request) {
-	const { pathname } = new URL(request.url)
-	const vfile = await readCurrentMdx(pathname)
-	const {
-		data: { headings },
-	} = vfile
+    const { pathname } = new URL(request.url)
+    const vfile = await readCurrentMdx({ pathname: pathname as string })
+    const {
+        data: { headings },
+    } = vfile
 
-	// TODO: Fix this type later
-	// @ts-ignore
-	const tocHeadings = headings.filter((heading) => heading.depth > 1)
+    // TODO: Fix this type later
+    // @ts-ignore
+    const tocHeadings = headings.filter((heading) => heading.depth > 1)
 
-	return tocHeadings
+    return tocHeadings
 }
 
-export { getMdxHeadings }
+async function getMdxHeadingsForV2Routes(request: Request) {
+    const { pathname } = new URL(request.url)
+    const vfile = await readCurrentMdx({ pathname: pathname as string, v2: true })
+    const {
+        data: { headings },
+    } = vfile
+
+    // TODO: Fix this type later
+    // @ts-ignore
+    const tocHeadings = headings.filter((heading) => heading.depth > 1)
+
+    return tocHeadings
+}
+
+export { getMdxHeadings, getMdxHeadingsForV2Routes }
