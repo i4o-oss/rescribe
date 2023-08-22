39640e00ef58b0a7ec9be7c65cee7932962f9042
Ilango <rajagopalilango@gmail.com>
2023-08-22 20:25:40 +0530

    👌 IMPROVE: Update pathname parser function - takes a pathname and returns an object with context of what to do

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,1 +1,2 @@
 export const BASE_PATH = '/rescribe'
+export const BASE_PATH_REGEX = /^\/rescribe\/?/
--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -1,6 +1,27 @@
-import { BASE_PATH } from './constants'
+import { BASE_PATH_REGEX } from './constants'
 
 export function parsePathname(pathname: string) {
-	const relativePath = pathname.replace(BASE_PATH, '')
-	return relativePath
+	const replaced = pathname.replace(BASE_PATH_REGEX, '')
+	const parts =
+		replaced === '' ? [] : replaced.split('/').map(decodeURIComponent)
+
+	if (parts.length === 0) {
+		return { root: true }
+	}
+
+	if (parts.length < 2 || parts[0] !== 'collections') return null
+
+	const collection = parts[1]
+	if (parts.length === 2) {
+		return { collection }
+	}
+	if (parts.length === 3 && parts[2] === 'new') {
+		return { collection, action: 'create' as const }
+	}
+	if (parts.length === 3 && parts[2] !== 'new') {
+		const slug = parts[2]
+		return { collection, action: 'edit' as const, slug }
+	}
+
+	return null
 }
