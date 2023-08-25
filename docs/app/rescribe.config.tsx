52565f916990f136b0b1c1fc260f599559badff2
Ilango <rajagopalilango@gmail.com>
2023-08-25 10:16:28 +0530

    👌 IMPROVE: Update rescribe config

--- docs/app/rescribe.config.tsx
+++ docs/app/rescribe.config.tsx
@@ -28,20 +28,25 @@ export const configObj = config({
 			schema: {
 				title: fields.text({
 					label: 'Title',
-					description: 'Post Title',
+					description: 'Title',
 				}),
 				slug: fields.slug({
 					label: 'Post URL',
+					description: 'URL of the post',
 				}),
 				excerpt: fields.text({
 					label: 'Excerpt',
+					description: 'A short description of the post',
 					multiline: true,
 				}),
 				published: fields.boolean({
-					label: 'Published',
+					label: 'Publish Post',
+					description:
+						'If the post should be published. Post will be saved as a draft by default.',
 				}),
 				publishedAt: fields.date({
 					label: 'Publish Date',
+					description: 'Set the publish date of the post',
 				}),
 			},
 		}),
