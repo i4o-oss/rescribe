ad5def1a9994876e8fcb4b518b9faef9a8ddd568
Ilango <rajagopalilango@gmail.com>
2023-09-04 17:36:03 +0530

    👌 IMPROVE: Update actions and loaders handler for new and edit pages

--- packages/server/src/helpers.ts
+++ packages/server/src/helpers.ts
@@ -1,6 +1,8 @@
-import type { Collection } from '@rescribe/core'
+import { refine } from '@conform-to/zod'
+import type { Collection, Schema, SchemaKey } from '@rescribe/core'
 import { REMIX_BASE_PATH } from '@rescribe/core'
 import fg from 'fast-glob'
+import { z } from 'zod'
 
 export async function readItemsInCollection(collection: Collection) {
 	const { path } = collection
@@ -8,4 +10,55 @@ export async function readItemsInCollection(collection: Collection) {
 	const entries = await fg(fullPath, { onlyFiles: true })
 
 	return entries
+}
+
+export function createSchema<key extends SchemaKey>({
+	collectionSchema,
+	options,
+}: {
+	collectionSchema: Schema<key>
+	options?: {
+		isSlugUnique?: (slug: string) => Promise<boolean>
+	}
+}) {
+	const collectionSchemaEntries = Object.keys(collectionSchema).map((key) => {
+		const item = collectionSchema[key as key]
+		// TODO: add better zod types for slug and url
+		switch (item.type) {
+			case 'date': {
+				return [key, z.coerce.date().default(new Date())]
+			}
+			case 'boolean': {
+				return [key, z.boolean().default(false)]
+			}
+			case 'text': {
+				if (key === 'slug') {
+					// TODO: do async validation for slug if a function that checks its uniqueness is passed
+					const slugSchema = z.string().superRefine((slug, ctx) =>
+						refine(ctx, {
+							validate: () => options?.isSlugUnique?.(slug),
+							message: 'slug is already taken',
+						})
+					)
+					return [key, slugSchema]
+				}
+			}
+			default: {
+				return [key, z.string().default('')]
+			}
+		}
+	})
+
+	return z.object(Object.fromEntries(collectionSchemaEntries))
+}
+
+export function getDefaults<Schema extends z.AnyZodObject>(schema: Schema) {
+	return Object.fromEntries(
+		Object.entries(schema.shape).map(([key, value]) => {
+			if (value instanceof z.ZodDefault) {
+				return [key, value._def.defaultValue()]
+			}
+			return [key, undefined]
+		})
+	)
 }
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,19 +2,15 @@ import type { ActionArgs, LoaderArgs } from '@remix-run/server-runtime'
 import { redirect } from '@remix-run/server-runtime'
 import { json } from '@remix-run/server-runtime'
 
-import {
-	REMIX_BASE_PATH,
-	generateZodSchema,
-	parsePathname,
-} from '@rescribe/core'
+import { parse } from '@conform-to/zod'
+import { REMIX_BASE_PATH, parsePathname } from '@rescribe/core'
 import type { Collections, Config } from '@rescribe/core'
 import fg from 'fast-glob'
 import matter from 'gray-matter'
 import fs from 'node:fs/promises'
 import YAML from 'yaml'
-import { zx } from 'zodix'
 
-import { readItemsInCollection } from './helpers'
+import { createSchema, getDefaults, readItemsInCollection } from './helpers'
 
 type LoaderHandlerArgs = LoaderArgs & {
 	config: Config<Collections>
@@ -42,6 +38,13 @@ export async function handleLoader({ config, request }: LoaderHandlerArgs) {
 			})
 		)
 		return json({ items: collectionItems })
+	} else if (params.collection && params.action === 'create') {
+		const collection = collections[params.collection]
+		const schema = createSchema({ collectionSchema: collection.schema })
+		const defaults = getDefaults(schema)
+		const data = schema.transform((val) => val).parse(defaults)
+
+		return json(data)
 	} else if (params.collection && params.action === 'edit') {
 		const collection = collections[params.collection]
 
@@ -90,10 +93,20 @@ export async function handleAction({ config, request }: ActionHandlerArgs) {
 	// TODO: data should have at least title and slug to be saved
 	if (params?.collection && params.action === 'create') {
 		const collection = collections[params.collection]
-		const formDataSchema = generateZodSchema(collection.schema)
-		const formData = await zx.parseForm(request, formDataSchema)
-		const createdAt = new Date().toISOString()
+		const formData = await request.formData()
+		const submission = parse(formData, {
+			schema: createSchema({
+				collectionSchema: collection.schema,
+				options: {
+					async isSlugUnique(slug) {
+						console.log(slug)
+						return true
+					},
+				},
+			}),
+		})
 
+		const createdAt = new Date().toISOString()
 		const frontmatterObj = {
 			title: formData.title,
 			slug: formData.slug,
@@ -104,15 +117,10 @@ export async function handleAction({ config, request }: ActionHandlerArgs) {
 		}
 
 		const frontmatter = YAML.stringify(frontmatterObj).trimEnd()
-		const markdown = formData.content.trim()
+		const markdown = submission?.value?.content
 
 		// TODO: find a better way to form markdown file content
-		const markdownFileContent = `---
-${frontmatter}
----
-
-${markdown}
-`
+		const markdownFileContent = `---\n${frontmatter}\n---\n\n${markdown}\n`
 
 		// TODO: directories have to exist in order to write file. figure out if directories exist and create them.
 		// TODO: if slug changes this will create a new file. keep track of old file name and rename or delete + recreate it.
@@ -120,7 +128,7 @@ ${markdown}
 		const fullPath = `${process.cwd()}${REMIX_BASE_PATH}/${collection.path.replace(
 			'*',
 			''
-		)}${formData.slug}.md`
+		)}${submission?.value?.slug}.md`
 		await fs.writeFile(fullPath, markdownFileContent)
 		//
 		return redirect(
@@ -128,33 +136,47 @@ ${markdown}
 		)
 	} else if (params?.collection && params.action === 'edit') {
 		const collection = collections[params.collection]
-		const formDataSchema = generateZodSchema(collection.schema)
-		const formData = await zx.parseForm(request, formDataSchema)
+		const formData = await request.formData()
+		const submission = await parse(formData, {
+			schema: createSchema({
+				collectionSchema: collection.schema,
+				options: {
+					async isSlugUnique(slug) {
+						console.log(slug)
+						return true
+					},
+				},
+			}),
+			async: true,
+		})
 
+		// read frontmatter from existing file and merge it with submission
+		// so any extra fields in the frontmatter like `createdAt` is not lost while overwriting the field
+		const filePath = `${process.cwd()}${REMIX_BASE_PATH}/${collection.path.replace(
+			'*',
+			''
+		)}${params.slug}.md`
+		const file = await fs.readFile(filePath, 'utf8')
+		const { data } = matter(file)
+		const metadata = YAML.parse(`${JSON.stringify(data)}\n`)
 		const frontmatterObj = {
-			title: formData.title,
-			slug: formData.slug,
-			excerpt: formData.excerpt,
-			publishedAt: formData.publishedAt,
-			published: formData.published,
+			...metadata,
+			...submission.value,
 		}
+		delete frontmatterObj.content
 
+		// get yaml string from json object
 		const frontmatter = YAML.stringify(frontmatterObj).trimEnd()
-		const markdown = formData.content.trim()
-
-		const markdownFileContent = `---
-${frontmatter}
----
-
-${markdown}
-`
-
+		// get content from submission
+		const markdown = submission?.value?.content
+		// form markdown file string and write it to file
+		const markdownFileContent = `---\n${frontmatter}\n---\n\n${markdown}\n`
 		// TODO: if slug changes this will create a new file. keep track of old file name and rename or delete + recreate it.
 		// TODO: this path only works for files with depth = 1 but glob allows nested directories. figure out how to get a working path regardless of how files are organized.
 		const fullPath = `${process.cwd()}${REMIX_BASE_PATH}/${collection.path.replace(
 			'*',
 			''
-		)}${formData.slug}.md`
+		)}${submission?.value?.slug}.md`
 		await fs.writeFile(fullPath, markdownFileContent)
 
 		return json({ message: 'updated' })
