af731e0ae2eefc97a5e5e1cb19282bce9e770d3d
Ilango <rajagopalilango@gmail.com>
2023-09-07 14:44:52 +0530

    👌 IMPROVE: Use blog package for Rescribe blog

--- docs/app/routes/blog.$.tsx
+++ docs/app/routes/blog.$.tsx
@@ -1,20 +1,39 @@
-import type { LoaderArgs } from '@remix-run/node'
-import { json } from '@remix-run/node'
+import type { LinksFunction, LoaderArgs } from '@remix-run/node'
 
-import { BlogLayout } from '@rescribe/blog'
-// import { createClient } from '@rescribe/server'
+import { Footer, Navbar } from '@i4o/rescribe'
+import { BlogOutlet } from '@rescribe/blog'
+import rescribeBlogStylesheet from '@rescribe/blog/main.css'
 import { configObj as config } from '~/rescribe.config'
+import { handleRescribeBlogLoader } from '~/utils/rescribe.server'
 
-export async function loader({ request }: LoaderArgs) {
-	// const rescribe = createClient(config)
-	// const item = await rescribe.blog.unique({ where: { slug: 'vicimus' } })
-	return json({})
+export const links: LinksFunction = () => [
+	{ rel: 'stylesheet', href: rescribeBlogStylesheet },
+]
+
+export async function loader(args: LoaderArgs) {
+	return handleRescribeBlogLoader({ ...args, config })
 }
 
 export default function BlogRoot() {
 	return (
-		<BlogLayout basePath='/blog' config={config}>
-			<div>Blog</div>
-		</BlogLayout>
+		<>
+			<Navbar />
+			<div className='flex min-h-[calc(100vh-10rem)] w-full flex-col mx-auto max-w-4xl justify-start items-center'>
+				<div className='container max-w-4xl p-6 lg:py-10 lg:px-0'>
+					<div className='flex h-48 flex-col items-start gap-4 md:flex-row md:items-center md:justify-between md:gap-8'>
+						<div className='flex flex-1 flex-col items-start justify-start space-y-4'>
+							<h1 className='text-foreground inline-block text-3xl font-extrabold tracking-tight lg:text-5xl'>
+								Blog
+							</h1>
+							<p className='text-foreground-subtle text-xl'>
+								Crafts, product updates, and technical details
+							</p>
+						</div>
+					</div>
+					<BlogOutlet />
+				</div>
+			</div>
+			<Footer />
+		</>
 	)
 }
--- docs/app/utils/rescribe.server.ts
+++ docs/app/utils/rescribe.server.ts
@@ -1,9 +1,18 @@
+import { handleBlogLoader } from '@rescribe/blog'
+import type { LoaderHandlerArgs } from '@rescribe/server'
+import { createClient } from '@rescribe/server'
 import { handleAction, handleLoader } from '@rescribe/server'
 
-export async function handleLoaderForRescribe(args) {
+export async function handleLoaderForRescribe(args: LoaderHandlerArgs) {
 	return await handleLoader(args)
 }
 
-export async function handleActionForRescribe(args) {
+export async function handleActionForRescribe(args: LoaderHandlerArgs) {
 	return await handleAction(args)
+}
+
+export async function handleRescribeBlogLoader(args: LoaderHandlerArgs) {
+	const client = createClient(args.config)
+	const blog = client['blog']
+	return await handleBlogLoader({ ...args, blog })
 }
