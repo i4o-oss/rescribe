4a66feddbb1c061f529e5f8afd48c8d35ee321d2
Ilango <rajagopalilango@gmail.com>
2023-09-16 23:52:31 +0530

    📦 NEW: Add test package for testing rescribe in remix v2

---
+++ test/app/utils/theme.server.ts
@@ -0,0 +1,34 @@
+import { createCookieSessionStorage } from '@remix-run/node'
+
+import { Theme, isTheme } from './theme-provider'
+
+const sessionSecret =
+	process.env.SESSION_SECRET || 'RESCRIBE_THEME_SESSION_SECRET'
+if (!sessionSecret) {
+	throw new Error('SESSION_SECRET must be set')
+}
+
+const themeStorage = createCookieSessionStorage({
+	cookie: {
+		name: '__rescribe_theme',
+		secure: true,
+		secrets: [sessionSecret],
+		sameSite: 'lax',
+		path: '/',
+		httpOnly: true,
+	},
+})
+
+async function getThemeSession(request: Request) {
+	const session = await themeStorage.getSession(request.headers.get('Cookie'))
+	return {
+		getTheme: () => {
+			const themeValue = session.get('theme')
+			return isTheme(themeValue) ? themeValue : null
+		},
+		setTheme: (theme: Theme) => session.set('theme', theme),
+		commit: () => themeStorage.commitSession(session),
+	}
+}
+
+export { getThemeSession }
