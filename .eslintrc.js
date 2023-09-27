afafe05202bdb70aea3917061b10bf82ebcd19cc
Ilango <rajagopalilango@gmail.com>
2023-09-27 07:15:46 +0530

    👌 IMPROVE: Update names of config packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
 	root: true,
 	// This tells ESLint to load the config from the package `eslint-config-custom`
-	extends: ['@rescribe/eslint-config'],
+	extends: ['@rescribejs/eslint-config'],
 }
