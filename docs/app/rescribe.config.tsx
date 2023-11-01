5b600c33eb2d92a9ef94556ac4c9c82409fb572d
Ilango <rajagopalilango@gmail.com>
2023-11-01 21:49:03 +0530

    👌 IMPROVE: Update rescribe config for docs

--- docs/app/rescribe.config.tsx
+++ docs/app/rescribe.config.tsx
@@ -23,6 +23,15 @@ export const configObj = config({
 				content: fields.document({
 					label: 'Content',
 				}),
+				published: fields.boolean({
+					label: 'Publish Page',
+					description:
+						'If the page should be published. Page will be saved as a draft by default.',
+				}),
+				publishedAt: fields.date({
+					label: 'Publish Date',
+					description: 'Set the publish date of the page',
+				}),
 				slug: fields.slug({
 					label: 'Page URL',
 					description: 'URL of the page',
