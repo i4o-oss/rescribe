88260628a3d21bc9397840615ac33dcbba287a6a
Ilango <rajagopalilango@gmail.com>
2023-11-06 19:16:13 +0530

    👌 IMPROVE: Update handle action function

--- packages/rescribe-server/src/server.ts
+++ packages/rescribe-server/src/server.ts
@@ -95,59 +95,61 @@ export async function handleAction({ config, request }: ActionHandlerArgs) {
 			async: true,
 		})
 
-		// TODO: data should have at least title and slug to be saved
-		if (params.action === 'create' && submission.value) {
-			const createdAt = new Date().toISOString()
-			const frontmatterObj = {
-				createdAt,
-				...submission.value,
+		if (submission.value) {
+			// TODO: data should have at least title and slug to be saved
+			if (params.action === 'create') {
+				const createdAt = new Date().toISOString()
+				const frontmatterObj = {
+					createdAt,
+					...submission.value,
+				}
+				// @ts-ignore
+				delete frontmatterObj.content
+
+				const frontmatter = YAML.stringify(frontmatterObj).trimEnd()
+				const markdown = submission?.value?.content
+
+				const markdownFileContent = `---\n${frontmatter}\n---\n\n${markdown}\n`
+
+				// TODO: if slug changes this will create a new file. keep track of old file name and rename or delete + recreate it.
+				const fullPath = getPath(collection, submission?.value?.slug)
+				const [filename] = fullPath.split('/').splice(-1)
+				const dirPath = fullPath.replace(filename, '')
+				fs.access(dirPath)
+					.then(async () => {
+						await fs.writeFile(fullPath, markdownFileContent)
+					})
+					.catch(async () => {
+						await fs.mkdir(dirPath, { recursive: true })
+						await fs.writeFile(fullPath, markdownFileContent)
+					})
+				const redirectUrl = `/rescribe/collections/${params.collection}/${submission?.value?.slug}`
+				return redirect(redirectUrl)
+			} else if (params.action === 'edit') {
+				// read frontmatter from existing file and merge it with submission
+				// so any extra fields in the frontmatter like `createdAt` is not lost while overwriting the field
+				const filePath = getPath(collection, params.slug)
+				const file = await fs.readFile(filePath, 'utf8')
+				const { data } = matter(file)
+				const metadata = YAML.parse(`${JSON.stringify(data)}\n`)
+				const frontmatterObj = {
+					...metadata,
+					...submission.value,
+				}
+				delete frontmatterObj.content
+
+				// get yaml string from json object
+				const frontmatter = YAML.stringify(frontmatterObj).trimEnd()
+				// get content from submission
+				const markdown = submission?.value?.content
+				// form markdown file string and write it to file
+				const markdownFileContent = `---\n${frontmatter}\n---\n\n${markdown}\n`
+				// TODO: if slug changes this will create a new file. keep track of old file name and rename or delete + recreate it.
+				const fullPath = getPath(collection, submission?.value?.slug)
+				await fs.writeFile(fullPath, markdownFileContent)
+
+				return json({ message: 'updated' })
 			}
-			// @ts-ignore
-			delete frontmatterObj.content
-
-			const frontmatter = YAML.stringify(frontmatterObj).trimEnd()
-			const markdown = submission?.value?.content
-
-			const markdownFileContent = `---\n${frontmatter}\n---\n\n${markdown}\n`
-
-			// TODO: if slug changes this will create a new file. keep track of old file name and rename or delete + recreate it.
-			const fullPath = getPath(collection, submission?.value?.slug)
-			const [filename] = fullPath.split('/').splice(-1)
-			const dirPath = fullPath.replace(filename, '')
-			fs.access(dirPath)
-				.then(async () => {
-					await fs.writeFile(fullPath, markdownFileContent)
-				})
-				.catch(async () => {
-					await fs.mkdir(dirPath, { recursive: true })
-					await fs.writeFile(fullPath, markdownFileContent)
-				})
-			const redirectUrl = `/rescribe/collections/${params.collection}/${submission?.value?.slug}`
-			return redirect(redirectUrl)
-		} else if (params.action === 'edit') {
-			// read frontmatter from existing file and merge it with submission
-			// so any extra fields in the frontmatter like `createdAt` is not lost while overwriting the field
-			const filePath = getPath(collection, params.slug)
-			const file = await fs.readFile(filePath, 'utf8')
-			const { data } = matter(file)
-			const metadata = YAML.parse(`${JSON.stringify(data)}\n`)
-			const frontmatterObj = {
-				...metadata,
-				...submission.value,
-			}
-			delete frontmatterObj.content
-
-			// get yaml string from json object
-			const frontmatter = YAML.stringify(frontmatterObj).trimEnd()
-			// get content from submission
-			const markdown = submission?.value?.content
-			// form markdown file string and write it to file
-			const markdownFileContent = `---\n${frontmatter}\n---\n\n${markdown}\n`
-			// TODO: if slug changes this will create a new file. keep track of old file name and rename or delete + recreate it.
-			const fullPath = getPath(collection, submission?.value?.slug)
-			await fs.writeFile(fullPath, markdownFileContent)
-
-			return json({ message: 'updated' })
 		}
 	}
 
