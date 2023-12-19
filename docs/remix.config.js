62dd4c90abaf021185bfb8e9ebd44cbbc1558aec
Ilango <rajagopalilango@gmail.com>
2023-12-19 12:26:35 +0530

    👌 IMPROVE: Set up docs for netlify

--- docs/remix.config.js
+++ docs/remix.config.js
@@ -1,5 +1,8 @@
+const adapter = require('@netlify/remix-adapter')
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+	...(process.env.NODE_ENV === 'production' ? adapter.config : undefined),
 	browserNodeBuiltinsPolyfill: {
 		modules: {
 			fs: true,
